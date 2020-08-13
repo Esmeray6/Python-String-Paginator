ecd9462260953367329fbeb2fb5da9ed50db8a6f
shivaco <22858781+shivaco@users.noreply.github.com>
2020-08-13 22:45:57 +0600

    Made it actually work properly
    This used to include empty elements in the returned list because of improper handling.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from typing import List
 def paginate(string: str, amount_of_chars: int) -> List[str]:
     pages = []
     mi = 0
-    for i in range(0, len(string) + 5):
+    for i in range(0, len(string)):
+        elem = ""
         if i % amount_of_chars == 0 and i != 0:
+            elem = string[mi:i]
             mi = i
-            pages.append(string[mi:i])
+            pages.append(elem)
     if mi < len(string):
-        pages.append(string[mi:])
+        elem = string[mi:]
+        pages.append(elem)
     return pages
